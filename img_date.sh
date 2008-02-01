1ad98e9817f0c4402cbd5c0ae32c8ae95a146b2c
Angel Olivera <redondos@gmail.com>
2008-02-01 20:36:38 +0100

    img_date: rewritten in a much better way, implemented "reverse" function

--- img_date.sh
+++ img_date.sh
@@ -1,44 +1,74 @@
 #!/bin/bash
 # TODO: Add support for .thm files describing videos
 #	Replace awk with cut
+
 if [ -z $1 ]; then
-	echo "usage: $0 EXPRESSION
-	Where EXPRESSION is a list of filenames separated by spaces."
+	echo "usage: $0 [ -r ] [ files ]"
+	echo "The optional "-r" flag reverses the changes."
+	exit 1
 fi
-for file in $@
-do
-	TS=`exif "$file" |grep 'Date.*digi'|head -n 1|awk -F "|" '{print $2}'`
-	if [ -n "$TS" ] ; then 
-	# We have EXIF data.
-		TS_date=$(echo $TS | awk '{print $1}')
-		TS_time=$(echo $TS | awk '{print $2}')
-		TS_date_y=$(echo $TS_date | awk -F":" '{print $1}')
-		TS_date_m=$(echo $TS_date | awk -F":" '{print $2}')
-		TS_date_d=$(echo $TS_date | awk -F":" '{print $3}')
-		TS_time_h=$(echo $TS_time | awk -F":" '{print $1}')
-		TS_time_m=$(echo $TS_time | awk -F":" '{print $2}')
-		TS_time_s=$(echo $TS_time | awk -F":" '{print $3}')
 
-		NEW_FILENAME="$TS_date_y $TS_date_m $TS_date_d T $TS_time_h $TS_time_m $TS_time_s"
-		NEW_FILENAME="${NEW_FILENAME// /}-${file/JPG/jpg}"
-		# file_timestamp=`echo $timestamp|sed 's/://g'|sed 's/ /-/'|cut -c -15`
-		echo "mv -v "$file" \"$NEW_FILENAME\""
-		echo touch -acmt $(expr substr $TS_date_y 3 2)$TS_date_m$TS_date_dT$TS_time_h$TS_time_m.$TS_time_s "$NEW_FILENAME"
-	else
-	# No EXIF data.
-		TS=$(stat --format="%y" "$file")
-		TS_date=$(echo $TS | awk '{print $1}')
-		TS_date=${TS_date//-/}
-		TS_time=$(echo $TS | awk '{print $2}')
-		TS_time=${TS_time//:/}
-		TS_time=$(echo $TS_time | cut -c 1-6)
+exif_ts() {
+	exif "$1" | awk -F\| '/Date.*orig/{print $2; quit}'
+}
 
-		NEW_FILENAME="$TS_date T $TS_time"
-		NEW_FILENAME="${NEW_FILENAME// /}-${file/JPG/jpg}"
+rename_ts() {
+	for file in "$@"; do
+		if [[ ! -e $file ]]; then
+			continue
+		fi
 
-		echo "mv -v "$file" \"$NEW_FILENAME\""
+		case "$file" in
+		*mpg|*MPG)
+			if [ -e "${file%.*}.thm" ]; then
+				ts=$(exif_ts "${file%.*}.thm")
+			elif [ -e "${file%.*}.THM" ]; then
+				ts=$(exif_ts "${file%.*}.THM")
+			else
+				ts=(stat --format="%y" "$file")
+			fi
+		;;
 
-	fi
-done
+		*)
+			ts=$(exif_ts "$file" 2>/dev/null)
+			if [[ -z "$ts" ]]; then
+				ts=$(stat --format="%y" "$file")
+				ts=${ts:0:19}
+			fi
+		;;
+		esac
 
-if [ -n "$1" ]; then echo "# This is a preview only. Run with $0 |sh to apply the changes."; fi
+		ts=$(sed 's/ *$//' <<< "$ts") # Why doesn't ${ts%% } work?
+		ts=${ts//[:-]/}
+		newfile=$(tr "A-Z" "a-z" <<< "$file")
+		newfile="${ts/ /T}-$newfile"
+
+		# YYYYmmddThhmmss-$file
+		echo "mv -v \"$file\" \"$newfile\""
+
+		# YYmmddhhmm.ss
+		# Seconds are superfluous and cause trouble
+		# echo touch -acmt ${ts:2:6}${ts:9:4}.${ts:10:2} \"$newfile\"
+
+		# YYmmddhhmm
+		echo touch -acmt ${ts:2:6}${ts:9:4} \"$newfile\"
+	done
+}
+
+restore() {
+	for file in "$@"; do
+		if [[ $file != ${file##*-} ]]; then
+			echo mv -v \"$file\" \"${file##*-}\"
+		fi
+	done
+}
+
+case "$1" in
+	-r)	shift;
+		restore "$@"
+	;;
+	*)	rename_ts "$@"
+	;;
+esac
+
+echo "# This is a preview only. Run with $0 |sh to apply the changes."
