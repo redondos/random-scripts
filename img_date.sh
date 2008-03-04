ec60eec379122b191dcb230549c3d0bae3760b88
Angel Olivera <redondos@gmail.com>
2008-03-04 23:02:44 +0100

    img_date refactored

--- img_date.sh
+++ img_date.sh
@@ -12,34 +12,45 @@ exif_ts() {
 	exif "$1" | awk -F\| '/Date.*orig/{print $2; quit}'
 }
 
+get_ts() {
+	if [[ ! -e $file ]]; then
+		return 1
+	fi
+
+	case "$file" in
+	*mpg|*MPG)
+		if [ -e "${file%.*}.thm" ]; then
+			ts=$(exif_ts "${file%.*}.thm")
+		elif [ -e "${file%.*}.THM" ]; then
+			ts=$(exif_ts "${file%.*}.THM")
+		else
+			ts=$(stat --format="%y" "$file")
+		fi
+	;;
+
+	*)
+		ts=$(exif_ts "$file" 2>/dev/null)
+		if [[ -z "$ts" ]]; then
+			ts=$(stat --format="%y" "$file")
+			ts=${ts:0:19}
+		fi
+	;;
+	esac
+
+	ts=$(sed 's/ *$//' <<< "$ts") # Why doesn't ${ts%% } work?
+	ts=${ts//[:-]/}
+
+	echo "$ts"
+}
+
 rename_ts() {
+
 	for file in "$@"; do
 		if [[ ! -e $file ]]; then
 			continue
 		fi
 
-		case "$file" in
-		*mpg|*MPG)
-			if [ -e "${file%.*}.thm" ]; then
-				ts=$(exif_ts "${file%.*}.thm")
-			elif [ -e "${file%.*}.THM" ]; then
-				ts=$(exif_ts "${file%.*}.THM")
-			else
-				ts=(stat --format="%y" "$file")
-			fi
-		;;
-
-		*)
-			ts=$(exif_ts "$file" 2>/dev/null)
-			if [[ -z "$ts" ]]; then
-				ts=$(stat --format="%y" "$file")
-				ts=${ts:0:19}
-			fi
-		;;
-		esac
-
-		ts=$(sed 's/ *$//' <<< "$ts") # Why doesn't ${ts%% } work?
-		ts=${ts//[:-]/}
+		ts=$(get_ts "$file")
 		newfile=$(tr "A-Z" "a-z" <<< "$file")
 		newfile="${ts/ /T}-$newfile"
 
@@ -63,9 +74,32 @@ restore() {
 	done
 }
 
+organize() {
+	for file in "$@"; do
+		if [[ ! -e $file ]]; then
+			continue
+		fi
+
+		ts=$(get_ts "$file")
+
+		dir=${ts:0:4}-${ts:4:2}-${ts:6:2}
+		if [[ -e $dir && ! -d $dir ]]; then
+			echo '# "$dir" exists but it is not a directory, skipping.'
+			continue
+		elif [[ ! -e $dir ]]; then
+			echo mkdir $dir "2>/dev/null"
+		fi
+
+		echo mv -v \"$file\" $dir
+	done
+}
+
 case "$1" in
-	-r)	shift;
+	-r)	shift
 		restore "$@"
+	;;
+	-o)	shift
+		organize "$@"
 	;;
 	*)	rename_ts "$@"
 	;;
